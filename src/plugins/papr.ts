46f4bfef474804037a08e3cb9e0ad54d205c0c76
Goshen <goshen009@gmail.com>
2026-03-17 04:10:27 +0100

    branded types!

--- src/plugins/papr.ts
+++ src/plugins/papr.ts
@@ -1,7 +1,18 @@
 import fastifyPaprPlugin from '@inaiat/fastify-papr'
 import { models } from '../types/papr.generated.js';
+import { SchemaType, SchemaOptions } from "papr";
 import { MongoClient } from 'mongodb';
 import fp from 'fastify-plugin';
+
+export type Brand<TSchema, K extends keyof TSchema> = TSchema[K] & { readonly __brand: K };
+
+export type BrandSchema<
+	TSchema extends [SchemaType<Record<string, unknown>, SchemaOptions<Record<string, unknown>>>, SchemaOptions<Record<string, unknown>>],
+	TOverrides extends { [K in keyof TOverrides]: K extends keyof TSchema[0] ? Brand<TSchema[0], K> : never }
+> = [
+  Omit<TSchema[0], keyof TOverrides> & TOverrides,
+  TSchema[1]
+];
 
 export default fp(async (fastify, opts) => {
   const client = await MongoClient.connect(fastify.config.db_uri);
